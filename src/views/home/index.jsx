a18bf2cf2b280f57ad25e3f9c597cebaa18c63bd
Arthur-Godoy <arthurgodoygomides@gmail.com>
2021-11-29 18:27:48 -0300

    Map refresh wen lat changes, and UX features

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,9 +1,10 @@
-import { Button, TextField, Grid, Typography } from '@material-ui/core';
+import { Button, TextField, Grid, Typography, CircularProgress } from '@material-ui/core';
+import Swal from 'sweetalert2';
 import { makeStyles } from '@material-ui/styles';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import weatherApi from '../../tools/api';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import api from '../../tools/api';
 import axios from 'axios';
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 
 const useStyles = makeStyles({
     background:{
@@ -28,9 +29,12 @@ const useStyles = makeStyles({
     textField:{
         padding: 0,
         height: 55,
-        borderRadius: '15 0 0 15',
+        borderRadius: '15px 0px 0px 15px',
         backgroundColor: 'white',
         width: 400,
+        "@media (max-width: 600px)":{
+            width: 200,
+        }
     },
 
     infosContainer:{
@@ -71,6 +75,12 @@ const useStyles = makeStyles({
         height: 600,
         zIndex: 1,
         top: 90,
+        "@media (max-width: 1380px)":{
+            height: 400,
+        },
+        "@media (max-width: 900px)":{
+            height: 800,
+        }
     },
 
     listContainer:{
@@ -79,42 +89,74 @@ const useStyles = makeStyles({
         color: 'black',
         position:'absolute',
         zIndex:3,
-        top: 846,
+        top: 886,
+        '@media(max-width: 1380px)':{
+            top: 680,
+        },
+        "@media (max-width: 900px)":{
+            top: 1080,
+        },
     },
-
-})
+});
 
 const Home = () => {
     const classes = useStyles();
     const [cityName, setCityName] = useState('--');
+    const [infoBoardCityName, setInfoBoardCityName] = useState('--');
     const [lat, setlat] = useState('--');
     const [lon, setLon] = useState ('--');
     const [cityTemp, setCityTemp] = useState('--');
     const [musicType, setMusicType] = useState('--');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        weatherApi.params.q = cityName;
+        api.weatherApi.params.q = cityName;
     }, [cityName])
-      
-    
 
     const getTemp = () =>{
-        axios.request(weatherApi).then(function (response) {
-            console.log(response.data);
+        setLoading(true)
+        axios.request(api.weatherApi).then(function (response) {
+            console.log(response.data)
+            setInfoBoardCityName(response.data['name'] + ', ' + response.data['sys']['country'])
+            setLon(response.data['coord']['lon'])
+            setlat(response.data['coord']['lat']) 
+            cityTemp === response.data['main']['temp'] ? (setLoading(false)):(setCityTemp(response.data['main']['temp']))
         }).catch(function (error) {
-            console.error(error);
+            setLoading(false);
+            Swal.fire({
+                icon: 'error',
+                title: 'Erro na Pesquisa',
+                text: 'Verifique o nome da cidade digitada',
+                showConfirmButton: false,
+            })
         });
     }
+
+    useEffect(() => {
+        if(cityTemp !== '--'){
+            if(cityTemp > 32){
+                setMusicType('funk')
+            }else if(cityTemp > 24){
+                setMusicType('pagode')
+            }else if(cityTemp > 16){
+                setMusicType('bossa nova')
+            }else{
+                setMusicType('lofi')
+            }
+        }
+        setLoading(false)
+    }, [cityTemp])
 
     return (
         <div>
             <div className={classes.background}>
                 <img className={classes.image} src="logo.png" alt="Geo Music" />
-                <TextField 
+                <TextField
                     className={classes.textField} 
                     label="Digite o nome da cidade" 
                     variant="filled" 
                     onChange={(e) =>{setCityName(e.target.value)}}
+                    onKeyDown={(e) => {e.key === 'Enter' ? (getTemp()):(console.log('.'))} }
                 />
                 <Button 
                     variant='contained' 
@@ -123,15 +165,16 @@ const Home = () => {
                         height: 56,
                         borderRadius: '0px 15px 15px 0px'
                     }}
+                    disabled={loading}
                     onClick={ () => {getTemp()}}
                 >
-                        <img src="icon-arrow.svg" alt="pesquisar"/> 
+                        {loading === true ? (<CircularProgress />):(<img src="icon-arrow.svg" alt="pesquisar"/> )}
                 </Button>
                 <div className={classes.infosContainer}>
                     <Grid container className={classes.infos}>
                         <Grid item md={3} xs={12} className={classes.infoGrid}>
                             <Typography variant='p' className={classes.subtitle}>City Name</Typography>
-                            <Typography variant='h5' className={classes.infoContent}>{cityName}</Typography>
+                            <Typography variant='h5' className={classes.infoContent}>{infoBoardCityName}</Typography>
                         </Grid>
 
                         <Grid item md={3} xs={12} className={classes.infoGrid}>
@@ -151,16 +194,15 @@ const Home = () => {
                     </Grid>
                 </div>
                 <div className={classes.map} id='map'>
-                    <MapContainer center={lat === '--' ? (['-20.4164','-42.9086']):([lat,lon])} zoom={lat === '--' ? (3):(13)} scrollWheelZoom={false} className={classes.map}>
+                    <MapContainer key={lat} center={lat === '--' || lon === '--' ? ([-20.4164,-42.9086]):([lat, lon])} zoom={13} scrollWheelZoom={true} className={classes.map}>
                         <TileLayer
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                        <Marker position={lat === '--' ? (['-20.4164','-42.9086']):([lat,lon])} />
+                        <Marker position={lat === '--' ? ([-20.4164, -42.9086]):([lat, lon])} />
                     </MapContainer>
                 </div>
                 <div className={classes.listContainer}>
-                    
                 </div>
             </div>
         </div>
